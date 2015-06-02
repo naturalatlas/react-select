9ae3a2cacead32dd9a2d6b89e7ebd9ca45d580d5
Brian Reavis <brian@thirdroute.com>
2015-06-02 13:28:15 -0600

    Added ability to set props on hidden input.

--- src/Select.js
+++ src/Select.js
@@ -845,7 +845,7 @@ var Select = React.createClass({
 
 		return (
 			<div ref="wrapper" className={selectClass}>
-				<input type="hidden" ref="value" name={this.props.name} value={this.state.value} disabled={this.props.disabled} />
+				<input type="hidden" ref="value" name={this.props.name} value={this.state.value} disabled={this.props.disabled} {...this.props.inputProps} />
 				<div className="Select-control" ref="control" onKeyDown={this.handleKeyDown} onMouseDown={this.handleMouseDown} onTouchEnd={this.handleMouseDown}>
 					{value}
 					{input}
